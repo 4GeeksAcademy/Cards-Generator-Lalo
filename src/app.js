a2a4ddb39ebfd7126e27f1232bc8f14ccb8d8e2d
Gonzalo Triviño <gonzalotcp@gmail.com>
2025-08-05 18:33:09 +0000

    generador de cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const generateRandomCard = () => {
+ 
+  const palos = ['♥️', '♦️', '♠️','♣️'];
+  const numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K'];
+  
+  
+  const randomPalo = palos[Math.floor(Math.random() * palos.length)];
+  const randomNumber = numbers[Math.floor(Math.random() * numbers.length)];
+ 
+  document.querySelector('.top-palo').innerHTML = randomPalo;
+  document.querySelector('.number').innerHTML = randomNumber;
+  document.querySelector('.bottom-palo').innerHTML = randomPalo;
+
 };
+
+const cardBtn = document.getElementById('card-btn');
+cardBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  generateRandomCard();
+});
+
+
+window.onload = function () {
+generateRandomCard();
+};
+
+
